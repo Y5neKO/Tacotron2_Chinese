a210655a02cb113928e892db686e1de178a1ce90
Y5neKO <41314905+Y5neKO@users.noreply.github.com>
2022-10-23 13:08:47 +0800

    Update hparams.py

--- hparams.py
+++ hparams.py
@@ -25,8 +25,8 @@ def create_hparams(hparams_string=None, verbose=False):
         # Data Parameters             #
         ################################
         load_mel_from_disk=False,
-        training_files='filelists/ljs_audio_text_train_filelist.txt',
-        validation_files='filelists/ljs_audio_text_val_filelist.txt',
+        training_files='filelists/training_test_pinyin.txt',
+        validation_files='filelists/training_test_pinyin.txt',
         text_cleaners=['english_cleaners'],
 
         ################################
